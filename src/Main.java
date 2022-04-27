33f5424703ff3e519b21cc333cd5713a8e9bd602
Francisco Garache <franciscogarach3@gmail.com>
2022-04-27 10:46:44 -0600

    new

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        double dato;
         System.out.println("Hello world!");
+        Scanner radio = new Scanner(System.in);
+        dato = radio.nextDouble();
 
-        Esfera esfera = new Esfera(45);
 
+
+        Esfera esfera = new Esfera(dato);
         System.out.println("Radio" + esfera.getRadio());
         System.out.println("Radio" + esfera.getDiametro());
         System.out.println("Radio" + esfera.getArea());
